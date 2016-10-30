f8eb5d21951c4d4e6b98c2c44a8ab8a74f403b11
OmarMohamedDev <omar.mohamed.dev@gmail.com>
2016-10-30 19:00:38 +0100

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+//
+//  Main.cpp
+//  CPlusPlusPractice
+//
+//  Created by Mohamed, Omar on 10/30/16.
+//
+//
+
+#include "Main.hpp"
---
+++ Main.hpp
@@ -0,0 +1,14 @@
+//
+//  Main.hpp
+//  CPlusPlusPractice
+//
+//  Created by Mohamed, Omar on 10/30/16.
+//
+//
+
+#ifndef Main_hpp
+#define Main_hpp
+
+#include <stdio.h>
+
+#endif /* Main_hpp */
